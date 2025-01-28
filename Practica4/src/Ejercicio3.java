afb6b1844ff41b1958ec68a58dbea9136e2bf298
Maria-Palma0731 <123046449@upq.edu.mx>
2025-01-28 16:31:32 -0600

    Ejercicio 3 Terminado

--- Practica4/src/Ejercicio3.java
+++ Practica4/src/Ejercicio3.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Ingresar la edad: ");
+        int e=scn.nextInt();
+        double p;
+        if (e < 4) {
+            p=0;
+        } else if (e <= 18) {
+            p=110;
+        } else {
+            p=190;
+        }
+        System.out.println("Precio de entrada: " + p);
+    }
 }
