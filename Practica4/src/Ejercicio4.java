e806b0e0c5e81ee304e395180d3eeeb922850ab6
Maria-Palma0731 <123046449@upq.edu.mx>
2025-01-28 16:40:14 -0600

    Ejercicio 4 terminado

--- Practica4/src/Ejercicio4.java
+++ Practica4/src/Ejercicio4.java
@@ -1,4 +1,24 @@
 import java.util.Scanner;
 
 public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Ingresar una palabra: ");
+        String c = scn.nextLine();
+
+        c = c.replaceAll("\\s+", "").toLowerCase();
+        int l = c.length();
+        boolean ep = true;
+        for (int i = 0; i < l / 2; i++) {
+            if (c.charAt(i) != c.charAt(l - i - 1)) {
+                ep = false;
+                break;
+            }
+        }
+        if (ep) {
+            System.out.println("Es palindromo.");
+        } else {
+            System.out.println("No es palindromo.");
+        }
+    }
 }
