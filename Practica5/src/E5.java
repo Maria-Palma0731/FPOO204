6bcdc3cd9801e1cff9e5862fd007cdc5f6f08d26
Maria-Palma0731 <123046449@upq.edu.mx>
2025-01-29 15:26:00 -0600

    E5 Terminado

--- Practica5/src/E5.java
+++ Practica5/src/E5.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
 
 public class E5 {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Introduce una frase: ");
+        String f = scn.nextLine();
+        System.out.print("Introduce una letra: ");
+        char l = scn.next().charAt(0);
+        int c = 0;
+        for (int i = 0; i < f.length(); i++) {
+            if (f.charAt(i) == l) {
+                c++;
+            }
+        }
+        System.out.println("La letra '" + l + "' aparece " + c + " veces en la frase.");
+    }
 }
--- Practica5/src/E6.java
+++ Practica5/src/E6.java
@@ -1,4 +1,9 @@
 import java.util.Scanner;
 
 public class E6 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+
+    }
 }
