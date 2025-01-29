0afd47c56c536584f3bce147d0190993fd9e6b28
Maria-Palma0731 <123046449@upq.edu.mx>
2025-01-29 15:14:16 -0600

    E2 Terminado

--- Practica5/src/E2.java
+++ Practica5/src/E2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class E2 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.print("Introduce un número entero positivo: ");
+        System.out.print("Introduce un numero: ");
         int n = scn.nextInt();
         for (int i = n; i >= 0; i--) {
             System.out.print(i);
