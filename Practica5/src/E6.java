091d735a5034ff4448e2ba0dce3f27f94dcfe141
Maria-Palma0731 <123046449@upq.edu.mx>
2025-01-30 22:59:57 -0600

    Cambio de logica/Terminado

--- Practica5/src/E6.java
+++ Practica5/src/E6.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
-
 public class E6 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int s = 0;
-        String o;
-
-        while (true) {
-            System.out.print("Escriba la bitacora de operaciones: ");
-            o = scn.nextLine();
-            if (o.isEmpty()) {
-                break;
+        int s = 5000;
+        String m;
+        boolean band = false;
+        System.out.println("Saldo: $" + s);
+        System.out.println("D: 100");
+        System.out.println("R: 50");
+        do {
+            System.out.print("Ingresar Operacion (D,R): ");
+            m = scn.nextLine();
+            if (m.equals("d") || m.equals("D")) {
+                System.out.println("Depositado $100.");
+                s += 100;
+            } else if (m.equals("r") || m.equals("R")) {
+                System.out.println("Retirado $50.");
+                s -= 50;
+            } else if (m.equals("")) {
+                band = true;
             }
-            String[] p = o.split(" ");
-            char t = p[0].charAt(0);
-            int c = Integer.parseInt(p[1]);
-            if (t == 'D') {
-                s+= c;
-            } else if (t == 'R') {
-                s-= c;
-            }
-        }
-        System.out.println("Saldo final: " + s);
+        } while (!band);
+        System.out.println("Saldo final: $" + s);
     }
 }
