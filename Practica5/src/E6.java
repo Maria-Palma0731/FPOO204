eba2ac6ea08a9b13a3476aee41ef4352b9373ce5
Maria-Palma0731 <123046449@upq.edu.mx>
2025-01-30 20:10:07 -0600

    Ej6 Terminado

--- Practica5/src/E6.java
+++ Practica5/src/E6.java
@@ -3,7 +3,24 @@ import java.util.Scanner;
 public class E6 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int s = 0;
+        String o;
 
-
+        while (true) {
+            System.out.print("Escriba la bitacora de operaciones: ");
+            o = scn.nextLine();
+            if (o.isEmpty()) {
+                break;
+            }
+            String[] p = o.split(" ");
+            char t = p[0].charAt(0);
+            int c = Integer.parseInt(p[1]);
+            if (t == 'D') {
+                s+= c;
+            } else if (t == 'R') {
+                s-= c;
+            }
+        }
+        System.out.println("Saldo final: " + s);
     }
 }
