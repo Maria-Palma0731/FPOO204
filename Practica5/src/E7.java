f113c59ec894a20bf124c25b89dbbbfe4caf6d05
Maria-Palma0731 <123046449@upq.edu.mx>
2025-01-30 23:00:17 -0600

    E7 Terminado

--- Practica5/src/E7.java
+++ Practica5/src/E7.java
@@ -1,4 +1,19 @@
 import java.util.Scanner;
 
 public class E7 {
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+        System.out.print("Base del arbol: ");
+        int b = scn.nextInt();
+        int f = 1, e = b - 1;
+        while (f <= (b * 2 - 1)) {
+            int tempE = e, tempF = f;
+            while (tempE-- > 0) System.out.print(" ");
+            while (tempF-- > 0) System.out.print("*");
+            System.out.println();
+            f += 2;
+            e--;
+        }
+        scn.close();
+    }
 }
